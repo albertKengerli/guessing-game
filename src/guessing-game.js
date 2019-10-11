1b8e5feafcacb77db4fa9f117ac8b206176791cc
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2019-10-11 19:47:19 +0300

    feat: add solution for task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.try = 0;
+      this.min = 0;
+      this.max = 0;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.try = Math.round(this.min + (this.max-this.min)/2);
+      return this.try;
     }
 
     lower() {
-
+      this.max = this.try;
     }
 
     greater() {
-
+      this.min = this.try;
+      
     }
 }
 
